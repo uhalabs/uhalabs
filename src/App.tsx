4a6bfe4addb51357a7dedf41b9abee2321501a04
Haribabu <harishnarpula1121@gmail.com>
2026-03-17 16:21:58 +0530

    removed

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import MainLandingPage from './Landingfiles/MainLandingPage'
-import ExcelProcessor from './Landingfiles/Analyser'
+
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<MainLandingPage />} />
-        <Route path="/analyser" element={<ExcelProcessor />} />
-
       </Routes>
     </BrowserRouter>
   )
